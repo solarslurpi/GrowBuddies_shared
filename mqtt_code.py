6d5e15d194861764a56b0565db8d8ad7fbc9803c
root <root@LAPTOP-4HTFESLT>
2024-07-09 12:58:04 -0700

    getting ready for mistbuddy_lite docker image build

--- mqtt_code.py
+++ mqtt_code.py
@@ -48,12 +48,9 @@ class MQTTClient:
         self.client_id = "".join(
             random.choice(string.ascii_lowercase) for i in range(10)
         )
+        # No Exceptions
+        self.client = mqtt.Client(client_id=self.client_id, clean_session=False,callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
 
-        try:
-            self.client = mqtt.Client(client_id=self.client_id, clean_session=False)
-        except Exception as e:
-            logger.debug(f"mqtt_code.init: Error creating MQTT client.  The error is {e}.")
-            raise e
         # This will be sent to subscribers who asked to receive the LWT for this mqtt client.
         self.client.will_set("/lwt", "offline", qos=1, retain=False)
         self.client.on_connect = self.on_connect
@@ -61,40 +58,48 @@ class MQTTClient:
         self.client.on_message = self.on_message
 
     def on_connect(self, client, userdata, flags, rc) -> int:
-        """mqtt_code.publish: Callback function that is called when the client connects to the MQTT broker."""
+        """Callback function that is called when the client connects to the MQTT broker."""
         logger.debug(
-            f"mqtt_code.publish: Connected to broker **{self.host}**.  The ClientID is {self.client_id}.  The result code is {str(rc)}"
+            f"Connected to broker {self.host}. ClientID: {self.client_id}. Result code: {rc}"
         )
         if rc != 0:
-            logger.error(
-                f"mqtt_code.publish: Received an unexpected error in on_connect.  The error is {rc_codes[rc]}"
-            )
-            return rc
+            logger.error(f"Unexpected error in on_connect: {rc_codes.get(rc, 'Unknown error code')}")
+        return rc
 
-    def on_disconnect(self, client, userdata, rc) -> int:
+    def on_disconnect(self, client, userdata, rc, properties) -> int:
+        """Callback function that is called when the client disconnects from the MQTT broker."""
         if rc != 0:
-            logger.error(
-                f"mqtt_code.publish: Received an unexpected disconnect.  The error is {rc_codes[rc]}"
-            )
+            logger.error(f"Received an unexpected disconnect. Error: {rc_codes.get(rc, 'Unknown error code')}")
         return rc
 
     def on_message(self, client, userdata, msg) -> None:
-        logger.debug(f"mqtt_code.publish: Received MQTT message: {msg.payload.decode('utf-8')}")
-
+        """Callback function that is called when a message is received from the MQTT broker."""
+        message = msg.payload.decode('utf-8')
+        logger.debug(f"Received MQTT message: {message}")
 
     def publish(self, topic, message, qos=1):
-        logger.debug(f"mqtt_code.publish: Publishing message {message} to topic {topic}.")
-        rc = self.client.publish(topic, message, qos)
-        return rc
+        """Publish a message to a specified MQTT topic."""
+        logger.debug(f"Publishing message '{message}' to topic '{topic}' with QoS {qos}.")
+        return self.client.publish(topic, message, qos)
 
     def disconnect(self):
         self.client.disconnect()
 
     def start(self):
-
-        self.client.connect(self.host)
-        self.client.loop_start()
+        try:
+            self.client.connect(self.host)
+            self.client.loop_start()
+        except OSError as e:
+            logger.error(f"Connection error: {e}")
+            raise  # Reraises the original OSError
+        except Exception as e:  # A more generic catch-all for unexpected errors
+            logger.error(f"Unexpected error: {e}")
+            raise  # Preserves the original exception
 
     def stop(self):
-        self.client.loop_stop()
-        self.client.disconnect()
+        try:
+            self.client.loop_stop()
+            self.client.disconnect()
+        except Exception as e:
+            logger.error(f"Failed to stop the MQTT client: {e}")
+            raise
--- power_code.py
+++ power_code.py
@@ -1,81 +1,123 @@
 import asyncio
 import logging
-# from dotenv import load_dotenv
-# load_dotenv()
+from pydantic import ValidationError, BaseModel, field_validator
 
 from mqtt_code import MQTTClient
 from logger_code import LoggerBase
-from settings_model import Settings
+from mistbuddy_lite_state_code import ServicesAddress, PowerMessages
 
 logger = LoggerBase.setup_logger("MistBuddyPower", logging.DEBUG)
 
+class PowerOnSeconds(BaseModel):
+    seconds_on: float
 
+    @field_validator('seconds_on')
+    def seconds_on_must_be_valid(cls, v):
+        if not isinstance(v, (float, int)):
+            raise TypeError('seconds_on must be a float')
+        if not (0.1 <= v <= 11.1 or 12 <= v <= 64800):
+            raise ValueError('seconds_on must be between 0.1 and 11.1 seconds or between 12 and 64800 seconds')
+        return v
+
+
+
+    @classmethod
+    def build_pulsetime_command(cls, power_command):
+        # The power_command must be in the format 'cmnd/<device_name>/POWER'
+        # If it doesn't pass the validation, an error is raised.
+        power_command = PowerMessages.match_power_message_or_raise_error(power_command)
+        # Split the topic by the '/'
+        parts = power_command.split("/")
+        # Replace the last part with 'PulseTime'
+        parts[-1] = "PulseTime"
+        # Join the parts back together to form the new topic
+        return "/".join(parts)
 
 class PowerBuddy:
-    def __init__(self, tent_name, settings: Settings):
-        self.settings = settings
+    '''
+    Operates the power switch (on/off) for Tasmotized devices associated with the MistBuddy name.
+    '''
+    def __init__(self, services_address:ServicesAddress, power_messages: PowerMessages):
+        '''mqtt messaging is used to send messages to the Tasmotized power switches. The MQTTClient class handles sending the messages.  In order to do that, the IP address (or hostname) of the mqtt broker is needed as well as the mqtt topics. These are located within the instances of the Settings model class.'''
         try:
-            host_ip = self.settings.global_settings.host_ip
-            self.power_topics = settings.pid_configs[tent_name].MistBuddy.mqtt_power_topics
+            # Manually validate
+            validated_services_address = ServicesAddress(address=services_address.address)
+            self.power_messages = PowerMessages(power_messages=power_messages.power_messages)
+        except ValidationError as e:
+            logger.error(f"Validation error: {e}")
+            raise
+        # Happens when one of the attributes passed in is None.
         except AttributeError as e:
-            logger.error(f"Error attempting to get the host_ip and power_topics from settings.  Error: {e}")
-            raise e
+            logger.error(f"Attribute error: {e}")
+            raise
         try:
-            self.mqtt_client = MQTTClient(host_ip)
-
+            self.mqtt_client = MQTTClient(validated_services_address.address)
         except Exception as e:
             logger.error(f"Error creating the MQTT client.  Error: {e}")
+            raise
+
+    def power_on(self, power_on_seconds: int | float) -> None:
+        try:
+            seconds_on = PowerOnSeconds(seconds_on=power_on_seconds).seconds_on
+        except ValidationError as e:
+            logger.error(f"Validation error: {e}")
             raise e
-
-    async def power_on(self, seconds_on: float) -> None:
-        for power_command in self.power_topics:
+        pulsetime_command = None # Initialize before the loop.
+        for power_command in self.power_messages.power_messages:
             try:
-                # TURN POWER ON.  The code is turning on a Tasmotized switch.  The code sends a Pulsetime.  This is the amount of time the switch should stay on until turning off.
+                # First Publish the Power on command to the Tasmotized switch.
+                self.mqtt_client.publish(power_command, 1, qos=1) # Generates exceptions
+                # Next send the pulsetime
+                pulsetime_command = PowerOnSeconds.build_pulsetime_command(power_command) # Does not generate exceptions
+                # PulseTime uses an algorithm defined at https://tasmota.github.io/docs/Commands/#control:
+                pulse_time = self._pulsetime_value(power_on_seconds=seconds_on) # Does not generate exceptions
 
-                # PulseTime<x> where x = 1..111 =  x  0.1 second increments. Thus the range is .1 to 11.1 seconds.
-                # PulseTime<x> where x = 112..64900 = - 100.  Thus the range is 12 to 6,4800 seconds.
-                # First Publish the Power on command
-                self.mqtt_client.publish(power_command, 1, qos=1)
-                # Split the topic by the '/'
-                parts = power_command.split("/")
-                # Replace the last part with 'PulseTime'
-                parts[-1] = "PulseTime"
-                # Join the parts back together to form the new topic
-                pulsetime_command = "/".join(parts)
-                # PulseTime command uses an algorithm defined at https://tasmota.github.io/docs/Commands/#control:
-                # PulseTime<x> where x = 112..64900 = - 100.  Thus the range is 12 to 6,4800 seconds.
-                if seconds_on >= 12:
-                    pulsetime_on = seconds_on + 100
-                # I must be missing something. .1 increments stop at 11.1 seconds. Add 100 starts at 12 seconds.  What happens between 11.1. and 12 seconds?
-                elif seconds_on < 12:
-                    # PulseTime<x> where x = 1..111 =  x  0.1 second increments. Thus the range is .1 to 11.1 seconds.
-                    pulsetime_on = seconds_on * 10 # e.g.: if pulsetime_on = 30 .1 second increments = 3 seconds.
-                # We will use numbers between 112 and 64900 for the PulseTime (e.g.: 112 - 100 = 12 seconds)
-                logger.debug(
-                    f"=+= POWER ON -> {seconds_on} seconds. Pulsetime value: {pulsetime_on}.=+="
-                )
+
                 # LET TASMOTA KNOW HOW LONG TO KEEP THE POWER ON.
-                self.mqtt_client.publish(pulsetime_command, pulsetime_on, qos=1)
-            except Exception as e:
+                self.mqtt_client.publish(pulsetime_command, pulse_time, qos=1)
+                logger.debug(
+                    f"=+= POWER ON -> {seconds_on} seconds. Pulsetime value: {pulse_time}.=+="
+                )
+            except (ValueError, TypeError) as e:
+                    error_message = (
+                        f"Invalid MQTT message format for command '{power_command}'  Error: {e}"
+                    )
+                    logger.error(error_message)
+                    raise ValueError(error_message) from e
+            except (RuntimeError, OSError) as e:
                 error_message = (
-                    f"Failed to publish MQTT message for command '{power_command}' "
-                    f"or '{pulsetime_command}'. Duration: {seconds_on} seconds. Error: {e}"
+                    f"MQTT communication error for command '{power_command}'  Duration: {seconds_on} seconds. Error: {e}"
                 )
                 logger.error(error_message)
-                raise RuntimeError(error_message)
+                raise RuntimeError(error_message) from e
+
+    def _pulsetime_value(self, power_on_seconds: float):
+        # Follow Tasmota's walgorithm for setting the pulsetime value.
+        if power_on_seconds >= 12:
+            return power_on_seconds + 100
+        elif power_on_seconds < 12:
+            return power_on_seconds * 10 # The same as / 0.1
 
     def start(self):
-        logger.debug("mistbuddy_power_code.start: Starting the MQTT client.")
-        self.mqtt_client.start()
+        try:
+            self.mqtt_client.start()
+            logger.debug("mqtt client started successfully")
+        except Exception as e:
+            logger.error(f"Error: {e} Failed to start the MQTT client.")
+            raise  # Optionally re-raise the exception if it should be handled further up
 
     def stop(self):
-        self.mqtt_client.stop()
-        logger.debug("mistbuddy_power_code.stop: Stopped the MQTT client.")
+        try:
+            self.mqtt_client.stop()
+            logger.debug("mqtt client stopped successfully.")
+        except Exception as e:
+            logger.error(f"Error: {e} Failed to stop the MQTT client.")
+            raise
 
 
     async def async_timer(self,interval, stop_event, seconds_on):
         while not stop_event.is_set():
-            await self.power_on(seconds_on)
+            self.power_on(seconds_on)
             await asyncio.sleep(interval)
 
     @property
